befd3154a8ffee6ef9063b57d0ea1a972cb6a6fe
Nitzan <nizanely@gmail.com>
2017-11-02 21:46:16 +0200

    update directories path in AppConfog class

--- app/src/main/java/com/itgelo/signin/app/AppConfig.java
+++ app/src/main/java/com/itgelo/signin/app/AppConfig.java
@@ -5,8 +5,8 @@ package com.itgelo.signin.app;
  */
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.114.25/android_api/login.php";
+    public static String URL_LOGIN = "http://10.0.2.2/android_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.114.25/android_api/register.php";
+    public static String URL_REGISTER = "http://10.0.2.2/android_api/register.php";
 }
